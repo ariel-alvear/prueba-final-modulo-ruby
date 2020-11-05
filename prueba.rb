23067ce4f5db71a692583c543e20d472c1fa2baa
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-11-05 13:09:46 -0300

    listo

--- prueba.rb
+++ prueba.rb
@@ -5,7 +5,6 @@ def concatenate_url_key(url, key)
     require 'openssl'
     require 'json'
     url_with_key = url + key
-    url_with_key
 
     url_api = URI(url_with_key)
 
@@ -19,11 +18,10 @@ def concatenate_url_key(url, key)
 
     response = http.request(request)
     response_hash = JSON.parse(response.read_body)
-    response_hash
 end
 
 =begin
-Al método debemos darle como argumento 1 la respuesta de la url de la nasa a la que queremos hicimos la request, y un número que será la cantidad de fotos que se mostrará.
+Al método debemos darle como argumento 1 la respuesta de la url de la nasa a la que queremos hicimos la request, y un número que será la cantidad de fotos al azar que se mostrará.
 Ingresados esos argumentos, el programa genera un archivo index.html con la cantidad de fotos aleatorias listadas en elementos <li>.
 En esta URL hay 856 fotos.
 =end
@@ -37,7 +35,7 @@ def build_web_page(hash, photos = 5)
 
     final_html = ["<html>","<head>", "</head>", "<body>", "<ul>"]
     bottom_html = ["</ul>", "</body>", "</html>"]
-    middle_html = filtered_url.sample(photos)
+    middle_html = filtered_url.sample(photos) #Recordar que el máximo de fotos es 856, y este método nos da una cantidad X al azar. 
     (middle_html.length).times { |y| final_html.push("\t<li><img src=\"#{middle_html[y]}\" width='700' height='500'></li>")}
     (bottom_html.length).times { |z| final_html.push(bottom_html[z]) }
 
@@ -47,25 +45,8 @@ end
 
 #al siguiente método al darle como argumento la url de la api de la nasa, nos devuelve un hash con cada cámara y cantidad de fotos que fueron sacadas.
 
-def photo_count(url)
-    require 'uri'
-    require 'net/http'
-    require 'openssl'
-    require 'json'
-
-    url = URI(url)
-    
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(url)
-    request["cache-control"] = 'no-cache'
-    request["postman-token"] = 'ccff926c-07c3-4696-0da0-7a689690bd5e'
-
-    response = http.request(request)
-    response_array_raw = JSON.parse(response.read_body)
-    response_array = response_array_raw['photos']
+def photo_count(hash)
+    response_array = hash['photos']
     filter_array = []
     (response_array.length).times do |x|
         response_array[x].each { |k, v| filter_array.push(v) if k == "camera" }
@@ -88,6 +69,6 @@ response_hash = concatenate_url_key("https://api.nasa.gov/mars-photos/api/v1/rov
 build_web_page(response_hash, 5)
 
 
-photo_count("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=icwWBO3w7LZOFTFwt4HheCVdRpPzlqacMxZDYyOL")
+photo_count(response_hash)
 
 
